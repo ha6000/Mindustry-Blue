08fb3a8edee61a15b3f4e306c75453516f0e69ec
Dev Harry <krugerh27@gmail.com>
2020-01-25 19:34:08 +0000

    fix

--- scripts/wind-mill.js
+++ scripts/wind-mill.js
@@ -1,6 +1,8 @@
+const updateEffectChance = 0.5;
+
 const powerEffect = newEffect(20, e => {
 	Draw.color(Color.white, Color.lightGray, e.fin()); //color goes from white to light gray
-	Lines.stroke(e.fout() * 3); //line thickness goes from 3 to 0
+	Lines.stroke(e.fout() * 3); //line thickness goes from 3 to 01
 	Lines.circle(e.x, e.y, e.fin() * 100); //draw a circle whose radius goes from 0 to 100
 });
 

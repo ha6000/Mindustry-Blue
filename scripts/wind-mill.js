071790c6e4bcd9faebc254a86ec46a81812d6b9d
Dev Harry <krugerh27@gmail.com>
2020-01-25 20:14:05 +0000

    test

--- scripts/wind-mill.js
+++ scripts/wind-mill.js
@@ -1,16 +1,17 @@
-const updateEffectChance = 0.1;
+const updateEffectChance = 0.5;
 
 const powerEffect = newEffect(20, e => {
-	Draw.color(Color.white, Color.lightGray, e.fin()); //color goes from white to light gray
-	Lines.stroke(e.fout() * 3); //line thickness goes from 3 to 01
-	Lines.circle(e.x, e.y, e.fin() * 100); //draw a circle whose radius goes from 0 to 100
+	Draw.color(Color.red, Color.lightGray, e.fin()); //color goes from white to light gray
+	Lines.stroke(e.fout() * 2); //line thickness goes from 3 to 01
+	Lines.circle(e.x, e.y, e.fin() * 500); //draw a circle whose radius goes from 0 to 100
 });
 
 const windMill = extendContent(SolarGenerator, 'wind-mill', {
 	update(tile) {
 		if (Mathf.chance(Time.delta() * updateEffectChance)) {
-			Effects.effect(powerEffect, tile)
+			Effects.effect(powerEffect, tile);
 		}
+		tile.entity.productionEfficiency = 1f;
 	}
 });
 print('Im loaded');

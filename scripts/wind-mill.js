4cc0c38dfb4eef0a825620e4ca58b240e4af24b5
Dev Harry <krugerh27@gmail.com>
2020-01-25 17:18:00 +0000

    test

--- scripts/wind-mill.js
+++ scripts/wind-mill.js
@@ -1,11 +1,13 @@
 const powerEffect = newEffect(20, e => {
-    Draw.color(Color.white, Color.lightGray, e.fin()); //color goes from white to light gray
-    Lines.stroke(e.fout() * 3); //line thickness goes from 3 to 0
-    Lines.circle(e.x, e.y, e.fin() * 100); //draw a circle whose radius goes from 0 to 100
+	Draw.color(Color.white, Color.lightGray, e.fin()); //color goes from white to light gray
+	Lines.stroke(e.fout() * 3); //line thickness goes from 3 to 0
+	Lines.circle(e.x, e.y, e.fin() * 100); //draw a circle whose radius goes from 0 to 100
 });
 
 const windMill = extendContent(SolarGenerator, 'wind-mill', {
 	update(tile) {
-		Effects.effect(powerEffect, tile);
+		if (Mathf.chance(Time.delta() * updateEffectChance)) {
+			Effects.effect(powerEffect);
+		}
 	}
 });

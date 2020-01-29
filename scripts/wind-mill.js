13629253392a268a7cad9523cf7b62d9da468f25
Dev Harry <krugerh27@gmail.com>
2020-01-29 17:09:04 +0000

    Added effect

--- scripts/wind-mill.js
+++ scripts/wind-mill.js
@@ -6,17 +6,15 @@ function getPowerEfficiency(powerPerSecond) {
 };
 
 const powerEffect = newEffect(20, e => {
-	Draw.color(Color.red, Color.lightGray, e.fin()); //color goes from white to light gray
+	Draw.color(Color.red, Color.blue, e.fin()); //color goes from white to light gray
 	Lines.stroke(e.fout() * 2); //line thickness goes from 3 to 01
-	Lines.circle(e.x, e.y, e.fin() * 500); //draw a circle whose radius goes from 0 to 100
+	Lines.circle(e.x, e.y, e.fin() * 100); //draw a circle whose radius goes from 0 to 100
 });
 
 const windMill = extendContent(SolarGenerator, 'wind-mill', {
 	update(tile) {
 		const doEffect = Mathf.chance(Time.delta() * updateEffectChance);
-		print(doEffect);
 		if (doEffect) {
-			print('Doing effect');
 			Effects.effect(powerEffect, tile);
 		};
 		tile.entity.productionEfficiency = efficiency;
